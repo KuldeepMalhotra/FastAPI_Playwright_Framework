3caa2f5f30a363611d1ef785ebfe8a7c1ab5132f
KuldeepMalhotra <kuldeepk927@gmail.com>
2025-09-02 13:50:41 +0530

    Git workflow isue fixed.

--- playwright.config.js
+++ playwright.config.js
@@ -45,9 +45,10 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:8000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: process.env.CI ? {
+    command: 'python3 server.py',
+    url: 'http://127.0.0.1:8000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  } : undefined,
 });
